3d1ba9d2a752fdca88a5c28887c4af35305b79cb
NinjazGY2497 <gyagau@gmail.com>
2026-08-31 02:37:06 -0400

    for orginization and more, moved some functionality from Board.java into "toLetterArray" method also, created the "toColorlessLetterArray" method

--- src/com/ninjaz/wordlesolver/CLI/Letter.java
+++ src/com/ninjaz/wordlesolver/CLI/Letter.java
@@ -1,9 +1,11 @@
 package com.ninjaz.wordlesolver.CLI;
 
+import java.util.Arrays;
+
 /** Each letter definitely contains three pieces of data:
  * - The letter (always uppercased)
  * - The color, represented as a character (TODO: MAKE THIS AN ENUM)
- *      'B' = Black, 'Y' = Yellow, 'G' = Green
+ *      'B' = Black, 'Y' = Yellow, 'G' = Green, '?' = Unknown
  * - The position of the letter in the word (0-indexed)
  *      Ex: 1st letter of the word is at Position 0
  *
@@ -19,13 +21,41 @@ public class Letter { // TODO: TO CONVERT TO A RECORD
         this.color = Character.toUpperCase(color);
         this.position = position;
 
-        if (color != 'B' && color != 'Y' && color != 'G') { // TODO: let's use an enum here instead, for color
-            throw new IllegalArgumentException("Invalid color: " + color);
+        if (this.color != 'B' && this.color != 'Y' && this.color != 'G' && this.color != '?') { // TODO: let's use an enum here instead, for color
+            throw new IllegalArgumentException("Invalid color: " + this.color);
         }
 
         if (position < 0) {
-            throw new IllegalArgumentException("Cannot have a negative position: " + position);
+            throw new IllegalArgumentException("Cannot have a negative position: " + this.position);
         }
+    }
+
+    /** Converts a String to a Letter array. However, all the Letter objects have an unknown color: '?' */
+    public static Letter[] toColorlessLetterArray(String str) {
+        Letter[] letterArray = new Letter[str.length()];
+        for (int i = 0; i < letterArray.length; i++) {
+            letterArray[i] = new Letter(str.charAt(i), '?', i);
+        }
+
+        return letterArray;
+    }
+
+    /** Converts a String to a Letter array. Takes in two parameters:
+     * A string of letters
+     * A string of characters representing the colors corresponding to each letter
+     * */
+    public static Letter[] toLetterArray(String lettersStr, String colorsStr) {
+        if (lettersStr.length() != colorsStr.length()) { // Make sure both passed in Strings are of same length
+            throw new IllegalArgumentException("The passed in 'lettersStr' and 'colorsStr' arguments must be of equal length");
+        }
+
+        // Loop through each character of the lettersStr and its corresponding color, adding them both to the letterArray
+        Letter[] letterArray = new Letter[lettersStr.length()];
+        for (int i = 0; i < letterArray.length; i++) {
+            letterArray[i] = new Letter(lettersStr.charAt(i), colorsStr.charAt(i), i);
+        }
+
+        return letterArray;
     }
 
     @Override
@@ -51,5 +81,9 @@ public class Letter { // TODO: TO CONVERT TO A RECORD
     @Override
     public int hashCode() {
         return java.util.Objects.hash(letter);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(toLetterArray("hello", "BBYGb")));
     }
 }
