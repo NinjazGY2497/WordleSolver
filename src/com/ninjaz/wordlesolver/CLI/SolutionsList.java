9a842bd08bf1a243bc094375173f58127d2961fa
NinjazGY2497 <gyagau@gmail.com>
2026-08-30 23:28:49 -0400

    created helper function "filterForLetter" for the algorithm

--- src/com/ninjaz/wordlesolver/CLI/SolutionsList.java
+++ src/com/ninjaz/wordlesolver/CLI/SolutionsList.java
@@ -15,6 +15,20 @@ public class SolutionsList {
     }
 
     // // ---- Algorithm ----
+    /** Remove's any Letter objects that don't have the target 'letter' attribute value
+     * Note: returns a copy */
+    private static Letter[] filterForLetter(char target, Letter[] letterArray) {
+        ArrayList<Letter> matchingLetters = new ArrayList<>();
+
+        for (Letter letter: letterArray) {
+            if (Character.toUpperCase(letter.letter) == Character.toUpperCase(target)) {
+                matchingLetters.add(letter);
+            }
+        }
+
+        return matchingLetters.toArray(Letter[]::new);
+    }
+
     // public boolean isSolution(String suspectWord) {
     //     for (Letter[] boardWord : boardWords) {
     //         // Remove duplicates (convert array to a HashSet and then back to an array)
@@ -28,5 +42,10 @@ public class SolutionsList {
     // ---- Other ----
     public ArrayList<String> getSolutions() {
         return solutions;
+    }
+
+    public static void main(String[] args) {
+        Letter[] myWord = {new Letter('A', 'B', 2), new Letter('B', 'B', 4), new Letter('C', 'B', 1), new Letter('D', 'B', 3), new Letter('D', 'B', 2)};
+        System.out.println(Arrays.toString(filterForLetter('D', myWord)));
     }
 }
