930ccfa5f1de45eb17788c3bad0f28c165e84eaf
NinjazGY2497 <gyagau@gmail.com>
2026-08-31 01:44:43 -0400

    created helper function "sortGYB" for the algorithm. However, i'm not sure if i'll need it anymore, so i've commented it out for now

--- src/com/ninjaz/wordlesolver/CLI/SolutionsList.java
+++ src/com/ninjaz/wordlesolver/CLI/SolutionsList.java
@@ -3,6 +3,7 @@ package com.ninjaz.wordlesolver.CLI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class SolutionsList {
     private ArrayList<Letter[]> boardWords;
@@ -21,7 +22,7 @@ public class SolutionsList {
         ArrayList<Letter> matchingLetters = new ArrayList<>();
 
         for (Letter letter: letterArray) {
-            if (Character.toUpperCase(letter.letter) == Character.toUpperCase(targetLetter)) {
+            if (Character.toUpperCase(letter.letter) == Character.toUpperCase(targetLetter)) { // If the Letter object is of the target letter character, we declare it as a match
                 matchingLetters.add(letter);
             }
         }
@@ -35,7 +36,7 @@ public class SolutionsList {
         ArrayList<Letter> matchingLetters = new ArrayList<>();
 
         for (Letter letter: letterArray) {
-            if (Character.toUpperCase(letter.color) == Character.toUpperCase(targetColor)) {
+            if (Character.toUpperCase(letter.color) == Character.toUpperCase(targetColor)) { // If the Letter is of the target color, we declare it a match
                 matchingLetters.add(letter);
             }
         }
@@ -43,10 +44,22 @@ public class SolutionsList {
         return matchingLetters.toArray(Letter[]::new);
     }
 
+    // /** Sorts the Letters based on their color attribute
+    //  * - The order is: Green, Yellow, Black */
+    // private static Letter[] sortGYB(Letter[] letterArray) {
+    //     ArrayList<Letter> colorSortedLetters = new ArrayList<>();
+    //
+    //     for (char color : new char[] {'G', 'Y', 'B'}) {
+    //         colorSortedLetters.addAll(List.of(filterForColor(color, letterArray))); // Adds all the 'G' letters, then all the 'Y' letters, then all the 'B' letters to the ArrayList
+    //     }
+    //
+    //     return colorSortedLetters.toArray(Letter[]::new);
+    // }
+
     // public boolean isSolution(String suspectWord) {
     //     for (Letter[] boardWord : boardWords) {
     //         // Remove duplicates (convert array to a HashSet and then back to an array)
-    //         HashSet<Letter> letterSet = new HashSet<>(Arrays.asList(boardWord));
+    //         HashSet<Letter> letterSet = new HashSet<>(List.of(boardWord));
     //         Letter[] uniqueLetters = letterSet.toArray(Letter[]::new);
     //
     //
@@ -60,6 +73,6 @@ public class SolutionsList {
 
     public static void main(String[] args) {
         Letter[] myWord = {new Letter('A', 'B', 2), new Letter('B', 'G', 4), new Letter('C', 'B', 1), new Letter('D', 'G', 3), new Letter('D', 'Y', 2)};
-        System.out.println(Arrays.toString(filterForColor('B', myWord)));
+        System.out.println(Arrays.toString(sortGYB(myWord)));
     }
 }
